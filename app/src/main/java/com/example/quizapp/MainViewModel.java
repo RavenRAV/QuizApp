388d7dacd71e8a9ec0918db1831a0ace35c3a7e1
RavenRav <akilaia@inbox.ru>
2020-02-27 22:15:55 +0300

    добавлен новый метод для отправления данных в RecyclerView

--- app/src/main/java/com/example/quizapp/MainViewModel.java
+++ app/src/main/java/com/example/quizapp/MainViewModel.java
@@ -1,23 +1,25 @@
 package com.example.quizapp;
 
-import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class MainViewModel extends ViewModel {
-    private int mCounter = 0;
+    private Integer mCounter = 0;
     public MutableLiveData<Integer> counter = new MutableLiveData<>();
+    public MutableLiveData<Integer> mResults = new MutableLiveData<>();
+
 
     void onIncrementClick() {
         mCounter++;
         counter.setValue(mCounter);
+        mResults.postValue(mCounter);
     }
 
     void onReductionClick() {
         mCounter--;
         counter.setValue(mCounter);
+        mResults.postValue(mCounter);
     }
 }
